ab11ddf9243840741f9a8ec388acf35032485972
rithiksinha <rithiksinha2000@gmail.com>
2021-11-02 15:45:31 +0530

    Initial Commit

---
+++ GitHubTest/GitHubTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubTestApp.swift
+//  GitHubTest
+//
+//  Created by Ritik Sinha on 02/11/21.
+//
+
+import SwiftUI
+
+@main
+struct GitHubTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
